e50adcea9aebc2902252420df3affae65917e9f3
semi <semi1907@outlook.com>
2024-07-17 13:30:23 +0300

    ArrayList Egzersizi

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace btkakademicalisma
 {
@@ -10,18 +11,37 @@ namespace btkakademicalisma
     {
         static void Main(string[] args)
         {
-            int[,] matris = new int[,] { { 12, 13, 14 }, { 15, 16, 17 } , {18, 19, 20} };
+            ArrayList arrayList = new ArrayList()
+            {55, 87,4, false, "Veli"};
 
-            for (int i = 0; i < matris.GetLength(0); i++)
-            {
-                for (int j = 0; j < matris.GetLength(1); j++)
-                {
-                    if (i == j)
-                        matris[i, j] = -1;
-                    Console.Write(matris[i, j]+" ");
-                }
-                Console.WriteLine("\n");
-            }
+
+            arrayList.Add("Ahmet");
+            arrayList.Add(250);
+            arrayList.Add(true);
+            arrayList.Add(85.4);
+            arrayList.Add('s');
+
+            int[] arr1 = new int[] { 22, 33, 44 };
+            arrayList.AddRange(arr1);
+
+            foreach(var i in arrayList)
+                Console.Write($"{i} ");
+
+            Console.WriteLine();
+            Console.WriteLine(arrayList[6]);
+
+            //Changing an element
+            var x = (int)arrayList[6];
+            Console.WriteLine(x+10);
+
+            //Deleting an element
+            arrayList.RemoveAt(6);
+            arrayList.Remove("Veli");
+            arrayList.RemoveRange(0, 1);
+
+            foreach (var i in arrayList)
+                Console.Write($"{i} ");
+
             Console.ReadKey();
         }
     }
