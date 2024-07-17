2f59bb44415148e9b8600ab9d1208a4259094b66
semi <semi1907@outlook.com>
2024-07-17 12:42:38 +0300

    Alıştırma tamamlandı

--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace btkakademicalisma
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter the size of array :");
+            int arr_size = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("--------------------------------------\n" + "Array :");
+            int[] arr = new int[arr_size];
+
+            var ran = new Random();
+
+            for (int i = 0; i < arr_size; i++)
+                arr[i] = ran.Next(1,10);
+
+            for(int i = 0;i < arr_size; i++)   
+                Console.Write(arr[i] +  " ");
+
+            Console.ReadKey();
         }
     }
 }
